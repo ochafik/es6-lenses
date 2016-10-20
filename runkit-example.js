2f2656161b188b758f2b599c5e066c9622f0cad5
Olivier Chafik <ochafik@users.noreply.github.com>
2016-10-20 13:09:05 +0100

    Create runkit-example.js

---
+++ runkit-example.js
@@ -0,0 +1,10 @@
+// Important: please select Node 6.x in the drop-down below, as this example requires ES6
+
+const {lens} = require("es6-lenses")
+
+const xy = lens(_ => _.x.y)
+
+let obj = {x: {y: 1}, z: 1}
+console.log(`Get: ${xy(obj)}`) // 1
+console.log(`Update: ${JSON.stringify(xy(obj, 10))}`)
+console.log(`Update from scratch: ${JSON.stringify(xy({}, 10))}`)
