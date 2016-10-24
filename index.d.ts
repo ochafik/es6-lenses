5af0539842762cfd70b356c9ae65933196b9c615
Olivier Chafik <olivier.chafik@gmail.com>
2016-10-24 21:29:30 +0100

    update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,8 @@
-export declare function selector(f: (_: any) => any): string[];
-
-export interface Lens<T, V> {
-    (target: T): V | undefined;
-    (target: T, value: V): T;
-    update(target: T, f: (value: V) => V): T;
+export declare abstract class Lens<T, V> {
+    abstract get(target: T): V | undefined;
+    abstract mutate(target: T, value: V): void;
+    abstract update(target: T, value: V): T;
+    andThen<W>(lens: Lens<V, W>): Lens<T, W>;
+    abstract after<A>(prefix: Lens<A, T>): Lens<A, V>;
 }
-export declare function lens<T, V>(path: ((_: T) => V) | PropertyKey[]): Lens<T, V>;
-export declare function mutatingLens<T, V>(path: ((_: T) => V) | PropertyKey[]): Lens<T, V>;
+export declare function lens<T, V>(f: (target: T) => V): Lens<T, V>;
