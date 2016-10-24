5fb6640706097ca3c2c714cdf28f123a02f529c1
Olivier Chafik <ochafik@users.noreply.github.com>
2016-10-24 10:43:12 +0100

    Update runkit-example.js

--- runkit-example.js
+++ runkit-example.js
@@ -5,6 +5,6 @@ const {lens} = require("es6-lenses")
 const xy = lens(_ => _.x.y)
 
 let obj = {x: {y: 1}, z: 1}
-console.log(`Get: ${xy(obj)}`) // 1
-console.log(`Update: ${JSON.stringify(xy(obj, 10))}`)
-console.log(`Update from scratch: ${JSON.stringify(xy({}, 10))}`)
+console.log(`Get: ${xy.get(obj)}`) // 1
+console.log(`Update: ${JSON.stringify(xy.update(obj, 10))}`)
+console.log(`Update from scratch: ${JSON.stringify(xy.update({}, 10))}`)
