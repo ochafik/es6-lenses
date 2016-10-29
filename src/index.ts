9e3f2c1c3a25d3e2196b68c588d9c10a8f2566cb
Olivier Chafik <olivier.chafik@gmail.com>
2016-10-29 14:53:25 +0100

    rename vars in index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,31 +5,32 @@ import {_, getLens} from './lens_proxy_handler';
 export {_} from './lens_proxy_handler';
 import {ObjectLens} from './object_lens';
 
-export function lens<T, V>(f: (((target: T) => V) | {} | Array<any>)): Lens<T, V> & Callable<T, V> {
-  const lens = getLens(f);
+export function lens<T, V>(definition: (((target: T) => V) | {} | Array<any>)): Lens<T, V> & Callable<T, V> {
+  const lens = getLens(definition);
   if (lens != null) {
     return makeLensCallable<T, V, any>(lens);
   }
-  if (f instanceof Function) {
-    f = f(_);
+  if (definition instanceof Function) {
+    definition = definition(_);
   }
-  return makeLensCallable<T, V, any>(composeLens<T, V>(f));
+  return makeLensCallable<T, V, any>(composeLens<T, V>(definition));
 }
 
-function composeLens<T, V>(result: any): Lens<T, V> {
-  const lens = getLens(result);
+function composeLens<T, V>(definition: any): Lens<T, V> {
+  const lens = getLens(definition);
   if (lens != null) {
     return lens;
-  } else if (result instanceof Array) {
-    return new ArrayLens<T, any>(result.map(v => {
+  } else if (definition instanceof Array) {
+    return new ArrayLens<T, any>(definition.map(v => {
       return composeLens<T, any>(v);
     }));
-  } else if (result instanceof Object && Object.getPrototypeOf(result) === Object.prototype) {
+  } else if (definition instanceof Object &&
+      Object.getPrototypeOf(definition) === Object.prototype) {
     // Object literal?
     return new ObjectLens<T, any>(
-        Object.keys(result).map(k =>
-          [k, composeLens<T, any>(result[k])] as [PropertyKey, Lens<T, any>]));
+        Object.keys(definition).map(k =>
+          [k, composeLens<T, any>(definition[k])] as [PropertyKey, Lens<T, any>]));
   } else {
-    throw new Error(`Invalid lens result (${typeof result})`);
+    throw new Error(`Invalid lens definition (${typeof definition})`);
   }
 }
